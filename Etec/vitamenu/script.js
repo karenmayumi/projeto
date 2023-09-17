3d6615be22bf96090c02392ceef9a3e0306bd594
kaemy <130376746+mariadesouzaw1@users.noreply.github.com>
2023-09-17 19:30:38 -0300

    save

---
+++ Etec/vitamenu/script.js
@@ -0,0 +1,5 @@
+const card1 = document.querySelector('.card-rec:nth-child(1)')
+const card2 = document.querySelector('.card-rec:nth-child(2)')
+const card3 = document.querySelector('.card-rec:nth-child(3)')
+
+card1.getElementsByClassName.backgroundColor = "blue"
