c2df1398aca026e26ecc1dde7ac2c152bc14a36b
kaemy <130376746+mariadesouzaw1@users.noreply.github.com>
2023-09-17 22:29:20 -0300

    pre-ultimo save

--- Etec/vitamenu/script.js
+++
@@ -1,8 +0,0 @@
-const card1 = document.querySelector('.card-rec:nth-child(1)')
-const card2 = document.querySelector('.card-rec:nth-child(2)')
-const card3 = document.querySelector('.card-rec:nth-child(3)')
-
-
-card1.addEventListener("mouseover",()=>{
-    card1.style.backgroundColor = "blue"
-})
