0aa96ec58dd90f9d35daf3a99bc9115a8862a18e
kaemy <130376746+mariadesouzaw1@users.noreply.github.com>
2023-09-17 21:01:10 -0300

    sessão depoimentos pronto

--- Etec/vitamenu/script.js
+++ Etec/vitamenu/script.js
@@ -2,4 +2,7 @@ const card1 = document.querySelector('.card-rec:nth-child(1)')
 const card2 = document.querySelector('.card-rec:nth-child(2)')
 const card3 = document.querySelector('.card-rec:nth-child(3)')
 
-card1.getElementsByClassName.backgroundColor = "blue"
+
+card1.addEventListener("mouseover",()=>{
+    card1.style.backgroundColor = "blue"
+})
