3bed00f743bd69282530c0bdd580cce718f3a45c
kaemy <130376746+mariadesouzaw1@users.noreply.github.com>
2023-10-29 09:32:51 -0300

    adding ddc's task

---
+++ ddc/js/games.js
@@ -0,0 +1,44 @@
+const quad = document.querySelector('.q')
+
+
+var num = Math.floor(Math.random()*100)
+console.log(num)
+var alerta = document.querySelector(".aviso")
+var tentativa = document.querySelector("input").value
+var caixa_tent = document.querySelector(".caixa_tent")
+var total_tents = 0
+var body = document.querySelector("body")
+
+function verificar(num,tentativa,total_tents,caixa_tent,alerta){
+  tentativa = document.querySelector("input").value
+  if(tentativa == num){
+    alerta.textContent = "O número inserido é o aleatório!"
+    
+
+    return true
+  }
+  else{
+    if(tentativa > num){
+      alerta.textContent = "O número aleatório é MENOR que o inserido!"
+    }
+    else{
+      alerta.textContent = "O número aleatório é MAIOR que o inserido!"
+      }
+
+        return false
+      }   
+}
+body.addEventListener("keydown",(event)=>{
+  if(event.key == "Enter"){
+    if (verificar(num,tentativa,total_tents,caixa_tent,alerta) == false){
+    total_tents++
+    caixa_tent.textContent = total_tents
+    }
+  }
+  if(event.key == "r"){
+    num = Math.floor(Math.random()*100)
+    console.log(num)
+    total_tents = 0
+    caixa_tent.textContent = total_tents
+  }
+})
---
+++ ddc/js/menu.js
@@ -0,0 +1,24 @@
+const menu = document.querySelector('#menu')
+const menu_nav = document.querySelector('#menu nav')
+const btn_nav = document.querySelector('#menu .label')
+const chest_img = document.querySelector('#menu img')
+var menu_chest = false
+function interaction_menu(){
+    if (menu_chest == false){
+        chest_img.src = '/img/chest-open.png'
+        menu_chest = true
+        menu_nav.style.maxHeight = '150px'
+
+    }else{
+        chest_img.src = '/img/chest-closed.png'
+        menu_chest = false
+        menu_nav.style.maxHeight = '0'
+
+    }
+}
+chest_img.addEventListener('click',()=>{
+    interaction_menu()
+})
+btn_nav.addEventListener('click',()=>{
+    interaction_menu()
+})
