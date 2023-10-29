6121da15ed0128d79f8d6901176bde715eebe81c
kaemy <130376746+mariadesouzaw1@users.noreply.github.com>
2023-10-29 12:07:26 -0300

    update game
    -caixa de entrada de informações adicionado;
    -imagens dos personagens adicionadas;
    -reorganização do tratamento de dados;

--- ddc/js/games.js
+++ ddc/js/games.js
@@ -1,44 +1,67 @@
-const quad = document.querySelector('.q')
+var aleatorio = Math.floor(Math.random()*100)
+console.log(aleatorio)
+
+const game = {
+  dicaOut : document.querySelector(".aviso"),
+  tentativaInp : parseInt(document.querySelector("input").value),
+  allTrysOut : document.querySelector(".caixa_tent"),
+  body : document.querySelector("body"),
+  allTrysjs : total_tents = 0
+}
+
+// ligação com as imagens
+const steve = {
+  perso : document.querySelector(".steve .perso"),
+  hearts : document.querySelector(".steve .heart")
+}
+const skel = {
+  perso : document.querySelector(".skeleton .perso"),
+  hearts : document.querySelector(".skeleton .heart")
+}
 
 
-var num = Math.floor(Math.random()*100)
-console.log(num)
-var alerta = document.querySelector(".aviso")
-var tentativa = document.querySelector("input").value
-var caixa_tent = document.querySelector(".caixa_tent")
-var total_tents = 0
-var body = document.querySelector("body")
-
-function verificar(num,tentativa,total_tents,caixa_tent,alerta){
-  tentativa = document.querySelector("input").value
-  if(tentativa == num){
-    alerta.textContent = "O número inserido é o aleatório!"
-    
-
+function verificar(aleatorio){
+  game.tentativaInp = parseInt(document.querySelector("input").value)
+  let tentativa = game.tentativaInp
+  if(tentativa == aleatorio){game.dicaOut.textContent = "O número inserido é o aleatório!"
     return true
   }
   else{
-    if(tentativa > num){
-      alerta.textContent = "O número aleatório é MENOR que o inserido!"
-    }
-    else{
-      alerta.textContent = "O número aleatório é MAIOR que o inserido!"
-      }
-
-        return false
-      }   
+    if(tentativa > aleatorio){game.dicaOut.textContent = "O número aleatório é MENOR que o inserido!"}
+    else{game.dicaOut.textContent = "O número aleatório é MAIOR que o inserido!"}
+    game.allTrysjs ++
+    game.allTrysOut.textContent = game.allTrysjs
+    return false
+  }   
 }
-body.addEventListener("keydown",(event)=>{
+
+
+function atack_skel(){
+  // comandos
+  skel.perso.url = './img/game/skel-a-1'
+}
+function atack_steve(){
+  // comandos
+}
+
+game.body.addEventListener("keydown",(event)=>{
   if(event.key == "Enter"){
-    if (verificar(num,tentativa,total_tents,caixa_tent,alerta) == false){
-    total_tents++
-    caixa_tent.textContent = total_tents
+    if (verificar(aleatorio) == false){
+
+      // codigo da animacao do steve
+
     }
   }
+
+
   if(event.key == "r"){
-    num = Math.floor(Math.random()*100)
-    console.log(num)
-    total_tents = 0
-    caixa_tent.textContent = total_tents
+    aleatorio = Math.floor(Math.random()*100)
+    console.log(aleatorio)
+    game.allTrysjs = 0
+    game.allTrysOut.textContent = game.allTrysjs
+
+    // adicionar os coracoes voltando ao normal
+
   }
 })
+
